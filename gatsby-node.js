c9bd4d9cdf6e3ceabea45e13c4abf30d80e75379
darbyjack <admin@glaremasters.me>
2022-03-01 10:12:32 -0600

    Updated for 3/1/22

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,7 @@ require('dotenv').config({
           method: 'POST',
           headers: {
               'x-api-key': 'P7LEOCujzt2RqSaWBeImz1spIoLq7dep7x983yQc',
-              'x-graphql-query-uuid': 'livestory---PostsWithGraph{"livestory_id":"h_846e5337e700c65ff85c849b0a86197e","startId":null}---580d0f56e1d52f797065034bd92f7f0858e93d0cb9398293bbf6c2716ad096a0'
+              'x-graphql-query-uuid': 'livestory---PostsWithGraph{"livestory_id":"h_03f0d7de5c9d6c55d8f338a3c3ba232a","startId":null}---e2451a5f7c563cbe6f9688458c5048f51fdfacf26dc216e18a72f11afc1b401d'
           },
       },
     );
